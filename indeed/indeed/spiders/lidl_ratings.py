d446de7ce24152f332d5becbf3cca18e116dca71
Nicolas Lattemann <6582485n@gmail.com>
2020-09-01 15:29:21 +0200

    cleaned the code, added a color print for exceptions

--- indeed/indeed/spiders/lidl_ratings.py
+++ indeed/indeed/spiders/lidl_ratings.py
@@ -1,7 +1,6 @@
 import scrapy
 import re
-from scrapy.linkextractors import LinkExtractor
-
+from ..Lib.prcolor import *
 
 class LidlRatingsSpider(scrapy.Spider):
     name = 'lidl_ratings'
@@ -13,19 +12,15 @@ class LidlRatingsSpider(scrapy.Spider):
         try:
             all_links_xpath = response.xpath("//a[@class='icl-Button icl-Button--tertiary icl-Button--lg']/@href").getall()
             regexed_links = [re.search(r"[?](.*)", x).group() for x in all_links_xpath if re.search(r"[?](.*)", x) != None]
-            try:
-                [self.all_links.append(x) for x in regexed_links if x not in self.all_links]
-            except Exception as e:
-                print(e)
-
+            [self.all_links.append(x) for x in regexed_links if x not in self.all_dlinks]
             link_to_go_to = self.start_urls[0] + self.all_links[-1]
 
             # Go to the last rating page that is linked on the current page
             url = response.urljoin(self.all_links[-1])
             yield scrapy.Request(url, callback=self.parse)
 
-            print(self.all_links)
         except Exception as e:
-            print(e)
+            prRed(e)
         # all_links_css = response.css("a.icl-Button::attr(href)").getall()
+        print(self.all_links)
         pass
