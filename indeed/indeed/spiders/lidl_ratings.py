0ab3a9cf3e5648eb8ba2e1145764c373e1d9d489
Nicolas Lattemann <6582485n@gmail.com>
2020-09-01 13:46:59 +0200

    getting the links for the different pages of ratings that are present in the starting url
    can only get the urls of the different rating pages that are present on the starting url (pages 1-5) . I need to follow the url of page 5 in order to get the next batch of urls (page 6-X)

--- indeed/indeed/spiders/lidl_ratings.py
+++ indeed/indeed/spiders/lidl_ratings.py
@@ -1,13 +1,23 @@
 import scrapy
+import re
 
 
 class LidlRatingsSpider(scrapy.Spider):
     name = 'lidl_ratings'
     allowed_domains = ['https://www.indeed.fr/cmp/Lidl/reviews']
-    start_urls = ['https://www.indeed.fr/cmp/Lidl/reviews/']
+    start_urls = ['https://www.indeed.fr/cmp/Lidl/reviews']
+    all_links = []
 
     def parse(self, response):
-        all_links_xpath = response.xpath("//a[@class='icl-Button icl-Button--tertiary icl-Button--lg']/@href").getall()
+        try:
+            all_links_xpath = response.xpath("//a[@class='icl-Button icl-Button--tertiary icl-Button--lg']/@href").getall()
+            [self.all_links.append(re.search(r"[?](.*)", x).group()) for x in all_links_xpath]
+            self.all_links.pop()
+            print("\n\n\n", self.all_links)
+            link_to_go_to = self.start_urls[0] + self.all_links[-1]
+            print(link_to_go_to)
+            # scrapy.Request(link_to_go_to, callback=self.parse(response))
+        except Exception as e:
+            print(e)
         # all_links_css = response.css("a.icl-Button::attr(href)").getall()
-        print("\n\n\n", all_links_xpath)
         pass
