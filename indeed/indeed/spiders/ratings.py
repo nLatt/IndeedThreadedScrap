fca6eb0933e1f3820e4c3faa34dd2e3b917c0c5d
Nicolas Lattemann <6582485n@gmail.com>
2020-09-02 17:37:16 +0200

    swapped to a linux venv

--- indeed/indeed/spiders/ratings.py
+++ indeed/indeed/spiders/ratings.py
@@ -1,10 +1,25 @@
 import scrapy
-
+from ..Lib.prcolor import *
 
 class RatingsSpider(scrapy.Spider):
     name = 'ratings'
-    start_urls = ['http://https://www.indeed.fr/cmp/Lidl/reviews/']
+    start_urls = ['https://www.indeed.fr/cmp/Lidl/reviews/']
+
+    custom_settings = {
+        "ITEM_PIPELINES": {
+               # 'indeed.pipelines.RatingUrlsPipeline': 400,
+        }
+    }
 
     def parse(self, response): # add an argument for the urls that get attributed to this process
+        # print(response.xpath("//div[@class='cmp-Review-container']/@data-tn-entityid").getall())
+
+        for rating in response.xpath("//div[@class='cmp-Review-container']"):
+            # prRed(rating.xpath("div[@class='cmp-Review-content']/descendant::*/text()").getall())
+            prRed(rating.xpath("div/div/a[@class='cmp-Review-titleLink']/text()").getall())
+            # prRed(rating)
+            # yield {
+            #     "tree": rating.xpath("").get()
+            #     }
 
         pass
