765521d0686e4f908435dd57db7fd400fd71e04c
Nicolas <6582485n@gmail.com>
2020-09-03 11:38:15 +0200

    created the structure of the dictthat will hold the parsed informations

--- indeed/indeed/spiders/ratings.py
+++ indeed/indeed/spiders/ratings.py
@@ -15,8 +15,17 @@ class RatingsSpider(scrapy.Spider):
         # print(response.xpath("//div[@class='cmp-Review-container']/@data-tn-entityid").getall())
 
         for rating in response.xpath("//div[@class='cmp-Review-container']"):
+            rating = {
+                # "rating": rating.xpath(),
+                "rating_content": {
+                    "rating_title": rating.xpath("div/div/a[@class='cmp-Review-titleLink']/text()").get(),
+                    "rating_author": rating.xpath("div/span[@class='cmp-ReviewAuthor']/text()").getall(),
+                    # "rating_text": rating.xpath(),
+                    # "rating pro_con": rating.xpath()
+                }
+            }
             # prRed(rating.xpath("div[@class='cmp-Review-content']/descendant::*/text()").getall())
-            prRed(rating.xpath("div/div/a[@class='cmp-Review-titleLink']/text()").getall())
+            prRed(rating)
             # prRed(rating)
             # yield {
             #     "tree": rating.xpath("").get()
