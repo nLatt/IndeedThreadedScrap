ff43c51388ecc85309a9e0d148ecdc849d0f2747
Nicolas <6582485n@gmail.com>
2020-09-03 15:21:43 +0200

    cleaned up the xpath, added every data feature I wanted

--- indeed/indeed/spiders/review.py
+++ indeed/indeed/spiders/review.py
@@ -17,11 +17,11 @@ class RatingsSpider(scrapy.Spider):
         pp = pprint.PrettyPrinter(indent=4)
 
         for review in response.xpath("//div[@class='cmp-Review-container']"):
-            text = [x for x in [x.strip() for x in  review.xpath("div/div/span[@class='cmp-ReviewAuthor']/descendant-or-self::*/text()").getall()] if len(x) > 1]
+            text = [x for x in [x.strip() for x in  review.xpath("descendant::*/span[@class='cmp-ReviewAuthor']/descendant-or-self::*/text()").getall()] if len(x) > 1]
             try:
                 review = {
-                    # "review_rating": review.xpath("div/div/div[@class='cmp-RatingStar s cmp-RatingStars--lg']/@aria-label").get(),
-                    "review_title": review.xpath("div/div/a[@class='cmp-Review-titleLink']/text()").get(),
+                    "review_title": review.xpath("descendant::*/a[@class='cmp-Review-titleLink']/text()").get(),
+                    "review_rating": review.xpath("descendant::*/div[@class='cmp-ReviewRating-text']/text()").get(),
                     "review_author": {
                         "job": text[0],
                         "status": text[1],
@@ -29,12 +29,12 @@ class RatingsSpider(scrapy.Spider):
                         "date": text[3],
                         },
 
-                    # "review_text": review.xpath(),
-                    # "review pro_con": review.xpath()
+                    "review_text": review.xpath("descendant::*/div[@class='cmp-Review-text']/descendant-or-self::*/text()").getall(),
+                    "review_pros_cons": {
+                        "review_pros": review.xpath("descendant::*/div[@class='cmp-ReviewProsCons-prosText']/span/span/text()").get(),
+                        "review_cons": review.xpath("descendant::*/div[@class='cmp-ReviewProsCons-consText']/span/span/text()").get()
+                        }
                     }
+                yield review
             except Exception as e:
                 prRed(e)
-
-            pp.pprint(review)
-
-        pass
